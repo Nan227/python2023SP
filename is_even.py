ee7dda882ee7aab47b45bdb879f435793a3f7e38
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-03-21 12:03:51 -0400

    works event code

---
+++ is_even.py
@@ -0,0 +1,43 @@
+#next week : 1, is_even :out 2 all even 3 any_even?
+
+
+#input[5,2,1,3] Output [False, True,False,False]
+
+# def is_even(values):
+#  # t=values[0] 
+#   for i in range(len(values)):
+#     if values[i] % 2 == 0:
+#       values[i]=True
+#       # print(values[i])
+#     else:
+#       values[i]=False
+#       # print(values[i])
+#   return values[i]
+
+#[x%]
+
+def is_even(values):
+  res = [] 
+  #print("result" + res)
+ # t=values[0] 
+  for x in values:
+    res.append(x%2 == 0)
+  return res
+
+def is_even2(values):
+  return [x %2 ==0 for x in values]
+
+  #range(len(values)):
+  #   result[i] =(values[i] % 2 ==0)
+  #   print(f"result{result} i =[i]")
+  # return result
+  #   #if values[i] % 2 == 0:
+  #     values[i]=True
+  #     # print(values[i])
+  #   else:
+  #     values[i]=False
+  #     # print(values[i])
+  # return values[i]
+test = [5, 3, 2 ,1]
+print(is_even(test))
+print(is_even2(test))
