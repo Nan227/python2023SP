0fb87d1e7c8e43193217495a0804679de56d3fa2
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-03-31 11:50:53 -0400

    prime update2

--- main_demo.py
+++ main_demo.py
@@ -1,13 +1,16 @@
 import lists_lib
-test = [2,4,6,9]
-print(f'test: {test}')
-print(f'is even2(test):{lists_lib.is_even2(test)}')
-print(f"any_even1(test) : {lists_lib.any_even(test)}")
-print(f"any_even2(test) : {lists_lib.any_even2(test)}")
 
-print(f"sqtotal(test) : {lists_lib.sqtotal(test)}")
+import prime_nubmber
+test = [2,4,3,7,21, 6,9]
+# print(f'test: {test}')
+# print(f'is even2(test):{lists_lib.is_even2(test)}')
+# print(f"any_even1(test) : {lists_lib.any_even(test)}")
+# print(f"any_even2(test) : {lists_lib.any_even2(test)}")
+
+# print(f"sqtotal(test) : {lists_lib.sqtotal(test)}")
 print("------------------------")
 print(test)
 print("------------------------")
-print(f"Is it all number even? \nAnswer: {lists_lib.all_evenf(test)}")
-print(f"Is it all number even? \nAnswer: {lists_lib.all_evenP(test)}")
+# print(f"Is it all number even? \nAnswer: {lists_lib.all_evenf(test)}")
+# print(f"Is it all number even? \nAnswer: {lists_lib.all_evenP(test)}")
+#print(f"Is it prime number? \nAnswer: {prime_nubmber.prime_number(test)}")
