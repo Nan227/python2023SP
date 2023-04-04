d3957203fc221a86022096e966cab46778bd0761
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-04-04 12:26:27 -0400

    learnning class and object

---
+++ apr4_demo.py
@@ -0,0 +1,15 @@
+#class = data type(eg. int, str, list, dict)
+# object = value(instance) ( eg. 5, " hell", [1,2,3]))
+d= datetime(2023,4,4)
+d.informat()
+d.strftime("%A, %B, %C")
+#int() =>0
+
+#list((1,2,3,hi)) => [1,2,3,..]
+#datetime.
+#STR = polo
+class mydate:
+  def __init__(self, yr,mo,day):
+    self.year = yr
+    self.month = mo
+    self.day =day
